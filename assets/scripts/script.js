9070421806e4e16f3338fe619583a077e5939730
Albator <4lb4t0R@protonmail.com>
2023-11-19 21:07:50 +0100

    ?

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+// SCRIPT TEST
+window.onclick = function (event) {
+  console.log("haha");
+};
+
+// SCRIPT POUR POP UP DE BIENVENUE
+alert("BIENVENUE SUR NOTRE SITE WEB ! 🌳");
+
+// SCRIPT POUR FOND MUSICAL
+let audio = new Audio("../assets/scripts/Sonic.mp3");
+audio.play();
